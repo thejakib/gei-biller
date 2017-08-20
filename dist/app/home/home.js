f5e8c894a97c75b3451574d94c6b3fe1fe89406d
thejakib <jaxe77@gmail.com>
2017-08-20 19:20:58 +0530

    ui done

--- dist/app/home/home.js
+++ dist/app/home/home.js
@@ -1,23 +1,57 @@
-angular.module('geibiller', [])
-.controller('items-controller', ['$scope', itemsController]);
+const Mousetrap = require('mousetrap');
+//const Excel = require('exceljs');
+const XLSX = require('xlsx');
+
+angular.module('geibiller', ['monospaced.elastic'])
+    .controller('items-controller', ['$scope', itemsController])
+    .controller('app-controller', appController);
 
 function itemsController($scope) {
+    var descTextarea = document.querySelector(".content .item:last-child textarea");
+
     $scope.items = [];
     $scope.qty = 0;
     $scope.rate = 0;
 
-    /*var item={
-        desc:"Nat. Jute Pouch Bags",
-        qty:5000,
-        rate:1.25,
-    };
-      
-    var item1={
-        desc:"Nat. Jute Sack",
-        qty:2000,
-        rate:3,
+    function addItem() {
+        var item = {
+            desc: $scope.desc,
+            qty: $scope.qty,
+            rate: $scope.rate
+        };
+        console.log("item-added", item);
+        $scope.items.push(item);
+        $scope.rate = 0;
+        $scope.qty = 0;
+        $scope.desc = "";
+        descTextarea.focus();
+        $scope.$digest();
     }
-      
-    $scope.items.push(item);
-    $scope.items.push(item1);*/
+    Mousetrap.bind('ctrl+shift+n', addItem);
+}
+
+
+function appController($scope) {
+    var saveFilename = "./output.xlsx";
+
+    $scope.save = function () {
+        var templateFilename = "dist/res/bill-template.xlsx";
+        var readOptions = {
+            raw: true,
+            cellStyles: true
+        };
+        var workbook = XLSX.readFile(templateFilename, readOptions);
+        readWorkbook(workbook);
+    }
+
+    function readWorkbook(workbook) {
+        var firstSheetName = workbook.SheetNames[0],
+            worksheet = workbook.Sheets[firstSheetName];
+        
+        worksheet['C12'] = {
+            v: 'Zen Enterprise'
+        }
+        
+        XLSX.writeFile(workbook, saveFilename);
+    }
 }
